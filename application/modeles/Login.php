8cafe4046e1872c4193fcbf352e43bfea4747a64
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-10-14 12:38:29 +0300

    Добавление модели авторизации

--- application/modeles/Login.php
+++ application/modeles/Login.php
@@ -8,8 +8,13 @@
 
 namespace application\modeles;
 
+use application\core\Model;
 
-class Login
+class Login extends Model
 {
-
+    public function checkAuth($login,$pass){
+        $result = $this->db->row("SELECT id,rights FROM users WHERE user = '$login' AND password = '$pass'");
+        setcookie('ID_USER',$result[0]['id']);
+        setcookie('RIGHT',$result[0]['rights']);
+    }
 }
