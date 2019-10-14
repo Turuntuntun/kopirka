2f9e2b0abce65bb73e05cc2ea74c7fd0c8a2db76
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-10-14 05:05:26 +0300

    Изменение путей в roure.php

--- application/configs/routes.php
+++ application/configs/routes.php
@@ -15,8 +15,16 @@ return [
         'controller' => 'task',
         'action' => 'add'
     ],
+    'task/delete' => [
+        'controller' => 'task',
+        'action' => 'delete'
+    ],
     '' => [
         'controller' => 'main',
+        'action' => 'index'
+    ],
+    'second_task' => [
+        'controller' => 'second_task',
         'action' => 'index'
     ]
 ];
