10b325cad2722088eb542ba53eaa5c80998654f3
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2019-10-14 05:00:24 +0300

    Добавление основного файла

---
+++ application/configs/routes.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Юра
+ * Date: 11.10.2019
+ * Time: 16:31
+ */
+
+return [
+    'login' => [
+        'controller' => 'login',
+        'action' => 'auth'
+    ],
+    'task/add' => [
+        'controller' => 'task',
+        'action' => 'add'
+    ],
+    '' => [
+        'controller' => 'main',
+        'action' => 'index'
+    ]
+];
